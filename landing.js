197f2fa0df9359fb611a6edd6bd711c19ef02398
Mausam <mauasam15171098@gmail.com>
2021-08-13 00:17:53 +0530

    completed header part

---
+++ landing.js
@@ -0,0 +1,20 @@
+function openingButton() {
+    
+    var itemContainer = document.getElementById('buttonContent')
+    var lineButton = document.querySelector('#hamburgerButton')
+    itemContainer.style.display = 'block'
+    var crossButton = document.querySelector('.crossLine')
+    lineButton.style.display = 'none'
+    crossButton.style.display = 'block'    
+}
+
+function defaultFunc() {
+    
+    var itemContainer = document.getElementById('buttonContent')
+    itemContainer.style.display = 'none'
+    var crossButton = document.querySelector('.crossLine')
+    crossButton.style.display = 'none' 
+    var lineButton = document.querySelector('#hamburgerButton')
+    lineButton.style.display = 'block'
+    location.reload();
+}
