34b631bb892e9f70e9817d144156f69abccd958d
Mausam <mauasam15171098@gmail.com>
2021-08-16 19:37:25 +0530

    completed the project made changes in forth and fifth section

--- landing.js
+++ landing.js
@@ -17,13 +17,24 @@ function defaultFunc() {
     var lineButton = document.querySelector('#hamburgerButton')
     lineButton.style.display = 'block'
     
-    // if (screen.width<960) {
-        
-    //     lineButton.style.display = 'block'
-    // }else{
-        
-    //     lineButton.style.display = 'none'
-    // }
-    // console.log(screen.width);
     location.reload()
 }
+
+var slide_index = 1;  
+displaySlides(slide_index);  
+function nextSlide(n) {  
+    displaySlides(slide_index += n);  
+}  
+function currentSlide(n) {  
+    displaySlides(slide_index = n);  
+}  
+function displaySlides(n) {  
+    var i;  
+    var slides = document.querySelectorAll(".showSlide");  
+    if (n > slides.length) { slide_index = 1 }  
+    if (n < 1) { slide_index = slides.length }  
+    for (i = 0; i < slides.length; i++) {  
+        slides[i].style.display = "none";  
+    }  
+    slides[slide_index - 1].style.display = "block";  
+} 
